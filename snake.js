558cbc4f00c16da0a33bbaf911512c6819200180
Atte Keinänen <atte.keinanen@gmail.com>
2015-04-04 13:03:24 +0300

    Start game when up arrow is pressed

--- snake.js
+++ snake.js
@@ -1,12 +1,10 @@
 // Snake by Patrick OReilly and Richard Davey
 // Twitter: @pato_reilly Web: http://patricko.byethost9.com
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update,render : render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'wappumato', { preload: preload, create: create, update: update,render : render });
 
 function preload() {
-
     game.load.image('ball','assets/sprites/shinyball.png');
-
 }
 
 var snakeHead; //head of snake sprite
@@ -14,6 +12,7 @@ var snakeSection = new Array(); //array of sprites that make the snake body sect
 var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
 var numSnakeSections = 30; //number of snake body sections
 var snakeSpacer = 6; //parameter that sets the spacing between sections
+var gameStarted = false;
 
 function create() {
 
@@ -27,14 +26,14 @@ function create() {
     snakeHead.anchor.setTo(0.5, 0.5);
 
     game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
-    
+
     //  Init snakeSection array
     for (var i = 1; i <= numSnakeSections-1; i++)
     {
         snakeSection[i] = game.add.sprite(400, 300, 'ball');
         snakeSection[i].anchor.setTo(0.5, 0.5);
     }
-    
+
     //  Init snakePath array
     for (var i = 0; i <= numSnakeSections * snakeSpacer; i++)
     {
@@ -45,14 +44,13 @@ function create() {
 
 function update() {
 
-    snakeHead.body.velocity.setTo(0, 0);
-    snakeHead.body.angularVelocity = 0;
+    if (gameStarted) {
+        snakeHead.body.velocity.setTo(0, 0);
+        snakeHead.body.angularVelocity = 0;
 
-    if (cursors.up.isDown)
-    {
         snakeHead.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(snakeHead.angle, 300));
 
-        // Everytime the snake head moves, insert the new location at the start of the array, 
+        // Everytime the snake head moves, insert the new location at the start of the array,
         // and knock the last position off the end
 
         var part = snakePath.pop();
@@ -66,17 +64,19 @@ function update() {
             snakeSection[i].x = (snakePath[i * snakeSpacer]).x;
             snakeSection[i].y = (snakePath[i * snakeSpacer]).y;
         }
-    }
 
-    if (cursors.left.isDown)
-    {
-        snakeHead.body.angularVelocity = -300;
+        if (cursors.left.isDown)
+        {
+            snakeHead.body.angularVelocity = -300;
+        }
+        else if (cursors.right.isDown)
+        {
+            snakeHead.body.angularVelocity = 300;
+        }
     }
-    else if (cursors.right.isDown)
-    {
-        snakeHead.body.angularVelocity = 300;
+    else {
+        if (cursors.up.isDown) gameStarted = true;
     }
-
 }
 
 function render() {
